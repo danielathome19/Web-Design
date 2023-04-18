30ac85da3918be81081414ae04b60fc4857d9913
Daniel James Szelogowski <dszelogowski@gmail.com>
2023-04-18 13:23:32 -0500

    Update 4/18

---
+++ external.js
@@ -0,0 +1,9 @@
+function calc() {
+    var out = document.getElementById("out");
+    out.innerText = "Hello";
+}
+
+function reset() {
+    var out = document.getElementById("out");
+    out.innerText = "";
+}
