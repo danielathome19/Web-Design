c3c3292c0a8d937940b14cdcfa5fdbb951ab5743
Daniel Szelogowski <dszelogowski@gmail.com>
2023-09-05 14:36:57 -0500

    Update

--- external.js
+++
@@ -1,9 +0,0 @@
-function calc() {
-    var out = document.getElementById("out");
-    out.innerText = "Hello";
-}
-
-function reset() {
-    var out = document.getElementById("out");
-    out.innerText = "";
-}
